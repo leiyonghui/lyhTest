d4ffe77dcd20d798062c23b64e9c7ffc8b7b41b4
leiyonghui <729186631@qq.com>
2022-02-14 18:13:48 +0800

    #

--- basics/basics/main.cpp
+++ basics/basics/main.cpp
@@ -1,8 +1,43 @@
-#include "core/test.h"
-#include <windows.h>
+#include<atomic>                                                                
+#include <stdio.h>
+using namespace std;
+
+atomic<int> a;
+int j;
+
+class AA {
+public:
+    AA(int aa = 0):a(aa) {
+
+    }
+
+    operator int() {
+        return a;
+    }
+
+    ~ AA(){
+
+    }
+public:
+    int a;
+};
+
+
+void testA(int a)
+{
+    printf("%d", a);
+}
+
 int main()
 {
-    test();
-    system("pause");
+    int n;
+
+    a.store(1, memory_order_acquire);
+    n = j;
+
+    {
+        AA* ta = new AA();
+        testA(*ta);
+    }
     return 0;
 }
