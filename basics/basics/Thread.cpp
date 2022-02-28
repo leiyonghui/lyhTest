60d1f65173cc2e99101fcdb72a922f33f928c4aa
leiyonghui <729186631@qq.com>
2022-02-28 17:19:43 +0800

    RingBuff

--- basics/basics/Thread.cpp
+++ basics/basics/Thread.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 TEST(example1)
 {
+    constexpr auto v = 10'000'000;
     std::thread a([] {
         cout<< " === " << endl;
      });
---
+++ include/core/RingBuff.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class RingBuff
+{
+    using uint32 = unsigned int;
+
+    RingBuff(const RingBuff&) = delete;
+    RingBuff& operator=(const RingBuff&) = delete;
+
+public:
+    explicit RingBuff(uint32 capacity);
+
+    ~RingBuff();
+
+    void write(char* buff, uint32 len);
+
+    void read(char* buff, uint32 len);
+
+    uint32 writableBytes() { return _capacity - _size; };
+
+    uint32 readableBytes() { return _size; }
+
+    uint32 size() { return _size; }
+
+    uint32 capacity() { return _capacity; }
+
+private:
+    void ensure(uint32 capacity);
+
+    uint32 _capacity;
+    uint32 _front;
+    uint32 _end;
+    uint32 _size;
+    char* _buff;
+};
