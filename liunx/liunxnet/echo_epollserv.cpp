053fcbbcde532f4b0c68b3dd6aaefc5474120661
leiyonghui <729186631@qq.com>
2022-03-01 23:49:18 +0800

    测试epoll

--- liunx/liunxnet/echo_epollserv.cpp
+++ liunx/liunxnet/echo_epollserv.cpp
@@ -6,9 +6,35 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
 
 #define BUF_SIZE 100
 #define EPOLL_SIZE 50
+
+void printEvent(int32 fd, int32 ev)
+{
+    std::ostringstream oss;
+    oss << "fd:" << fd << " "<<ev<<" events:";
+    if (ev & EPOLLIN)
+        oss << "IN ";
+    if (ev & EPOLLPRI)
+        oss << "PRI ";
+    if (ev & EPOLLOUT)
+        oss << "OUT ";
+    if (ev & EPOLLHUP)
+        oss << "HUP ";
+    if (ev & EPOLLRDHUP)
+        oss << "RDHUP ";
+    if (ev & EPOLLERR)
+        oss << "ERR ";
+    if (ev & EPOLLRDHUP)
+        oss << "NVAL ";
+    std::string str = oss.str();
+    printf("%s\n", str.c_str());
+}
 
 int32 echoEpollServ(int32 port)
 {
@@ -24,6 +50,11 @@ int32 echoEpollServ(int32 port)
 
 
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+	int32 optval = 1;
+	::setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR,
+		&optval, static_cast<socklen_t>(sizeof optval));
+
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -40,7 +71,7 @@ int32 echoEpollServ(int32 port)
     event.events = EPOLLIN;
     event.data.fd = serv_sock;
     epoll_ctl(epfd, EPOLL_CTL_ADD, serv_sock, &event);
-
+    cout << "--start--"<< port << endl;
     while (1)
     {
         event_cnt = epoll_wait(epfd, ep_events, EPOLL_SIZE, -1);
@@ -52,6 +83,9 @@ int32 echoEpollServ(int32 port)
 
         for (i = 0; i < event_cnt; i++)
         {
+            const auto& ev = ep_events[i];
+            cout << endl;
+            printEvent(ep_events[i].data.fd, ev.events);
             if (ep_events[i].data.fd == serv_sock)
             {
                 adr_sz = sizeof(clnt_adr);
@@ -62,7 +96,7 @@ int32 echoEpollServ(int32 port)
                 epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sock, &event);
                 printf("connected client: %d \n", clnt_sock);
             }
-            else
+            else if(ev.events & EPOLLIN)
             {
                 str_len = read(ep_events[i].data.fd, buf, BUF_SIZE);
                 if (str_len == 0)    // close request!
@@ -76,6 +110,9 @@ int32 echoEpollServ(int32 port)
                 {
                     write(ep_events[i].data.fd, buf, str_len);    // echo!
                 }
+            }
+            else if (ev.events & EPOLLOUT)
+            {
 
             }
         }
