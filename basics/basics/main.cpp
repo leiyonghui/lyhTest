1ddcea9afcc57c3dddd9ee0202333a61c9e0d266
leiyonghui <729186631@qq.com>
2022-03-07 22:31:03 +0800

    fixed

--- basics/basics/main.cpp
+++ basics/basics/main.cpp
@@ -1,5 +1,6 @@
 #include <atomic>                                                                
 #include <stdio.h>
+#include <vector>
 #include <iostream>
 #include <chrono>
 #include "constructor.h"
@@ -9,18 +10,44 @@
 #include "core/Types.h"
 #include "destructor.h"
 #include "StdPtr.h"
+#include <tuple>
+#include <variant>
+#include <iostream>
 
 using namespace core;
+using namespace std;
 //using namespace std;
 //using namespace std::chrono;
 //using namespace std::literals;
+
+class AA {
+
+public:
+	AA(int bb, bool dd):b(bb), d(dd) {
+
+	}
+
+	int b;
+	bool d;
+};
 
 
 int main()
 {
 	//testDestructor();
 	//testThread();
-	testShare();
+	//testShare();
+	//cout << 'f' << 'n' << 'r';
+	using arg = std::variant<int, bool>;
+
+	auto tp = std::make_tuple(1, 2);
+	std::apply([](auto&& ...args) {
+		((cout << args << " "), ...) << endl;
+		}, tp);
+	auto aa = std::make_from_tuple<AA>(std::move(tp));
+	cout << aa.b << "   " << aa.d << endl;
+
+	std::vector<std::tuple<arg, arg, arg, arg>> a;
     system("pause");
     return 0;
 }
