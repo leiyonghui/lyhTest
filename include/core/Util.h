65ca4205ee54c045ef01522a2a602d06028c692c
leiyonghui <729186631@qq.com>
2022-03-08 23:55:39 +0800

    #fix

--- include/core/Util.h
+++ include/core/Util.h
@@ -11,4 +11,46 @@ namespace util
 	const T CEmpty<T>::Empty = T();
 
 
+	template<class T>
+	class CSwitch
+	{
+		using Values = std::vector<T>;
+
+		std::vector<Values> _t;
+		short _index;
+
+	public:
+		CSwitch() : _index(0) {
+			_t.resize(2);
+			_t[0].reserve(8);
+			_t[1].reserve(8);
+		}
+
+		CSwitch(const Values& t1, const Values& t2) : _t{ t1, t2 }, _index(0) {
+
+		}
+
+		Values& first() { return _t[_index]; }
+
+		Values& back() { return _t[1 ^ _index]; }
+
+		void exchange() {
+			_index ^= 1;
+		}
+
+		void push(const T& value) {
+			_t[1 ^ _index].push_back(value);
+		}
+
+		template<class Iter>
+		void insert(const Iter& first, const Iter& end) {
+			auto& values = _t[1 ^ _index];
+			values.insert(values.end(), first, end);
+		}
+
+		void inster(const Values& adds) {
+			auto& values = _t[1 ^ _index];
+			values.inster(values.end(), adds.begin(), adds.end());
+		}
+	};
 }
