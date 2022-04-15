9129ceb79a5497b174e9878f03fc32bd98f889fa
leiyonghui <729186631@qq.com>
2022-04-16 01:54:48 +0800

    整理

--- Basics/Basics/main.cpp
+++ Basics/Basics/main.cpp
@@ -1,11 +1,14 @@
 #include <atomic>                                                                
 #include <stdio.h>
+#include "core/RingBuff.h"
 #include "headers.h"
 #include "template.h"
 
 int main()
 {	
 	testthread::testThread();
+    core_log_error("ssss");
+    CRingBuff buff(12);
     system("pause");
     return 0;
 }
